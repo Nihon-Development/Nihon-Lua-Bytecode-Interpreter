59332c3e77f072004a76afdae7d039404a7c0b5a
ImmuneLion318 <54381449+ImmuneLion318@users.noreply.github.com>
2021-04-28 21:12:17 -0400

    Update FileSystem.h

--- Nihon Lua Bytecode Interperter/Nihon Lua Bytecode Interperter/FileSystem.h
+++ Nihon Lua Bytecode Interperter/Nihon Lua Bytecode Interperter/FileSystem.h
@@ -7,18 +7,9 @@
 using namespace std;
 using namespace CallCheck;
 
-string GetDllPath(const char* Addon)
-{
-	HMODULE hModule = GetModuleHandleA("Nihon Lua Bytecode Interperter.dll");
-	if (hModule == NULL) return "";
-	char buffer[MAX_PATH + 1];
-	GetModuleFileNameA(hModule, buffer, MAX_PATH);
-	std::string::size_type pos = std::string(buffer).find_last_of("\\/");
-	string file = std::string(buffer).substr(0, pos).append("\\Workspace\\").append(Addon);
-	if (!file.empty())
-		return file;
-	return "error";
-}
+/*
+ * Never Gonna Be Finished
+*/
 
 void RegisterFileSystem() {
 
